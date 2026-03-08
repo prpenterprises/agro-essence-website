d76620bb29f95de9b95a1ea0072edd8b53b2d943
Preyansh <prpenterprises13@gmail.com>
2026-03-09 01:44:05 +0530

    Added SEO to website

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,11 @@ import StatsBar from "../components/StatsBar";
 import Network from "../components/Network";
 import MarketPresence from "../components/MarketPresence";
 
+export const metadata = {
+  title: "AgroEssence | Organic Fertilizers & Gardening Supplies",
+  description:
+    "Buy premium vermicompost, neem khali, cocopeat, fertilizers, plant protection solutions, and garden tools from AgroEssence.",
+};
 
 export default function Home() {
   return (
